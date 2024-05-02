5119696ceeb05980364d5848b386c20656672e3a
FCKNGUNNER <168190611+FCKNGUNNER@users.noreply.github.com>
2024-05-02 15:54:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,15 @@ from transformers import pipeline
 # Загрузка модели Llama-3
 saiga_pipeline = pipeline("text-generation", model="IlyaGusev/saiga_llama3_8b")
 
-# Функция для генерации текста с использованием модели Llama-3
-def generate_text(prompt):
-    return saiga_pipeline(prompt, max_length=100, do_sample=True, temperature=0.9)[0]['generated_text']
+# Функция для генерации ответа с использованием модели Llama-3
+def generate_response(prompt):
+    # Генерация текста с помощью модели Llama-3
+    generated_text = saiga_pipeline(prompt, max_length=100, do_sample=True, temperature=0.9)[0]['generated_text']
+    return generated_text
 
 # Главная часть приложения
-st.title('Генератор текста с помощью модели Llama-3')
-prompt = st.text_input("Введите ваш запрос:", "Начните вводить текст...")
-if st.button('Сгенерировать'):
-    generated_text = generate_text(prompt)
-    st.write(generated_text)
+st.title('Генератор ответов от Ламы')
+prompt = st.text_input("Ваш вопрос:", "Задайте вопрос Ламе...")
+if st.button('Ответить'):
+    response = generate_response(prompt)
+    st.write(f"Лама: {response}")
